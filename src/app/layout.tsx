c569a720fa8f4b0fa2424d774dfd5c0e03538f82
sharet9446 <spees97@naver.com>
2025-03-14 11:39:46 +0900

    1단계 미완료

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +14,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link href="/" className="nav-link">
+              Home(SSG)
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link href="/news" className="nav-link">
+              News(ISR)
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/blogs" className="nav-link">
+              Blogs(SSR)
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/todolist" className="nav-link">
+              TodoList(CSR)
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <body>{children}</body>
     </html>
   );
 }
